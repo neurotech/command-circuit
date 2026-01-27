253b679ea3895ab364023797cc207f4f810f8de8
Tim Douglas <neurotech@gmail.com>
2026-01-27 17:31:35 +1100

    Adjust TextInput font size

--- src/components/ui/text-input.tsx
+++ src/components/ui/text-input.tsx
@@ -18,7 +18,7 @@ export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
         <input
           ref={ref}
           {...props}
-          className="rounded-sm border border-zinc-600 bg-zinc-700/80 p-2 font-mono text-[11px] text-zinc-400 shadow-sm transition-colors focus:border-indigo-500 focus:bg-zinc-600 focus:text-zinc-50 focus:outline-none focus:ring-4 focus:ring-indigo-500/25 focus:selection:bg-zinc-800 disabled:cursor-not-allowed disabled:select-none disabled:border-zinc-900 disabled:bg-zinc-900/40 disabled:text-zinc-600 data-[valid=false]:border-rose-400 data-[valid=false]:bg-rose-800/20 data-[valid=false]:text-rose-400 data-[valid=false]:focus:bg-rose-950/20 data-[valid=false]:focus:ring-red-500/25"
+          className="rounded-sm border border-zinc-600 bg-zinc-700/80 p-2 font-mono text-sm text-zinc-400 shadow-sm transition-colors focus:border-indigo-500 focus:bg-zinc-600 focus:text-zinc-50 focus:outline-none focus:ring-4 focus:ring-indigo-500/25 focus:selection:bg-zinc-800 disabled:cursor-not-allowed disabled:select-none disabled:border-zinc-900 disabled:bg-zinc-900/40 disabled:text-zinc-600 data-[valid=false]:border-rose-400 data-[valid=false]:bg-rose-800/20 data-[valid=false]:text-rose-400 data-[valid=false]:focus:bg-rose-950/20 data-[valid=false]:focus:ring-red-500/25"
           data-valid={valid}
         />
       </div>
